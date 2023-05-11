2cac900f270343cc3c94afb4c2e767f8ce054a74
Facir <kazakov8070@gmail.com>
2023-05-11 12:57:20 +0300

    Add signup form

--- blog/form.py
+++ blog/form.py
@@ -1,5 +1,6 @@
 from django import forms
-from pytils.translit import slugify
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import *
 
 
@@ -10,3 +11,14 @@ class AddPostForm(forms.ModelForm):
         widgets = {
             'tags': forms.CheckboxSelectMultiple,
         }
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': "form-control"}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': "form-control"}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .form import AddPostForm
+from .form import AddPostForm, RegisterUserForm
 from .models import *
 from .utils import *
 
@@ -84,16 +82,17 @@ class BlogSignIn(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Регистрация!')
+        c_def = self.get_user_context(title='Вход')
         return dict(list(context.items()) + list(c_def.items()))
 
 
-class BlogSignUp(DataMixin, ListView):
+class BlogSignUp(DataMixin, CreateView):
+    form_class = RegisterUserForm
     template_name = 'blog/signup.html'
-    model = Blog
+    success_url = reverse_lazy('signin')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Вход!')
+        c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
 
